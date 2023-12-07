a24aac043daf11169f146e7976b4bc69040453fc
Vladimir Nastavshev <homer1990@ya.ru>
2023-12-07 14:50:54 +0300

    Lesson18_ScoreTable

--- Lesson13.cpp
+++ Lesson13.cpp
@@ -2,51 +2,74 @@
 
 using namespace std;
 
-class Vector 
-{
-private:
-    double x = 0;
-    double y = 0;
-    double z = 0;
+//Создайте класс, полями которого будут имя игрока и количество набранных этим игроком очков.
+// 
+//Узнайте у пользователя, сколько игроков он хочет добавить, и создайте динамический массив необходимого размера.Получите от пользователя имена игроков и набранные ими очки и сохраните в массиве.
+// 
+//Отсортируйте массив по убыванию количества очков, набранных игроками.
+// 
+//Выведите все имена и очки игроков в отсортированном виде.
 
+class ScoreTable
+{
 public:
-    // Конструктор
-    Vector(double x, double y, double z) : x(x), y(y), z(z) 
+    string name;
+    int score;
+
+    ScoreTable() : name(""), score(0) 
+    {}
+    ScoreTable(string name, int score) : name(name), score(score)
     {}
 
-    // Методы для получения координат вектора
-    double GetX() 
-    {
-        return x;
-    }
-
-    double GetY() 
-    {
-        return y;
-    }
-
-    double GetZ() 
-    {
-        return z;
-    }
-
-    // Метод для вычисления длины вектора
-    double Length()
-    {
-        return sqrt(x * x + y * y + z * z);
-    }
 };
 
-int main()
+
+void SortPlayers(ScoreTable* l, ScoreTable* r)
 {
-    // Создание вектора
-    Vector v(2, 7, 0.95);
+    int sz = r - l;
+    if (sz <= 1) return;
+    bool swapped = true;
+    while (swapped) 
+    {
+        swapped = false;
+        for (ScoreTable* i = l; i + 1 < r; i++) 
+        {
+            if (i->score < (i + 1)->score) 
+            {
+                swap(*i, *(i + 1));
+                swapped = true;
+            }
+        }
+        r--;
+    }
+}
 
-    // Вывод координат вектора
-    cout << "Vector (" << v.GetX() << ", " << v.GetY() << ", " << v.GetZ() << ")" << '\n';
+int main() {
+    int NumberOfPlayers;
+    cout << "Players number: ";
+    cin >> NumberOfPlayers;
 
-    // Вывод длины вектора
-    cout << "Vector length: " << v.Length() << '\n';
+    // Создание динамического массива
+    ScoreTable* Players = new ScoreTable[NumberOfPlayers];
+
+    // Получение данных об игроках
+    for (int i = 0; i < NumberOfPlayers; i++) {
+        cout << "Player " << (i + 1) << " - Name: ";
+        cin >> Players[i].name;
+        cout << "Player " << (i + 1) << " - Score: ";
+        cin >> Players[i].score;
+    }
+    // Сортировка массива по убыванию очков
+    SortPlayers(Players, Players + NumberOfPlayers);
+
+    // Вывод результатов
+    cout << "Sort players:\n";
+    for (int i = 0; i < NumberOfPlayers; i++) 
+    {
+        cout << Players[i].name << ": " << Players[i].score << '\n';
+    }
+    // Освобождение памяти
+    delete[] Players;
 
     return 0;
 }
